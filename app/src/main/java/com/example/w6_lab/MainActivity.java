cdb3972b4e1e8d2b6120e482c8aa3a4f6148c90d
Raygur <gurray0101@gmial.com>
2024-10-20 16:59:32 +0800

    edit:English content

--- app/src/main/java/com/example/w6_lab/MainActivity.java
+++ app/src/main/java/com/example/w6_lab/MainActivity.java
@@ -41,17 +41,17 @@ public class MainActivity extends AppCompatActivity {
         btn_mora.setOnClickListener(view -> {
             // 判斷玩家是否輸入姓名
             if (ed_name.length() < 1) {
-                tv_text.setText("請輸入玩家姓名");
+                tv_text.setText("insert player name");
             } else {
                 // 顯示玩家姓名與出拳
-                tv_name.setText(String.format("名字\n%s", ed_name.getText().toString()));
+                tv_name.setText(String.format("name\n%s", ed_name.getText().toString()));
 
                 if (btn_scissor.isChecked()) {
-                    tv_mmora.setText("我方出拳\n剪刀");
+                    tv_mmora.setText("Palyer\nScissors");
                 } else if (btn_stone.isChecked()) {
-                    tv_mmora.setText("我方出拳\n石頭");
+                    tv_mmora.setText("Palyer\nRock");
                 } else {
-                    tv_mmora.setText("我方出拳\n布");
+                    tv_mmora.setText("Palyer\nPaper");
                 }
 
                 // 使用亂數產生電腦出拳，值為 0~2
@@ -59,11 +59,11 @@ public class MainActivity extends AppCompatActivity {
 
                 // 顯示電腦出拳
                 if (computer_random == 0) {
-                    tv_cmora.setText("電腦出拳\n剪刀");
+                    tv_cmora.setText("Bot\nScissors");
                 } else if (computer_random == 1) {
-                    tv_cmora.setText("電腦出拳\n石頭");
+                    tv_cmora.setText("Bot\nRock");
                 } else {
-                    tv_cmora.setText("電腦出拳\n布");
+                    tv_cmora.setText("Bot\nPaper");
                 }
 
                 // 判斷勝負
@@ -71,19 +71,19 @@ public class MainActivity extends AppCompatActivity {
                         (btn_stone.isChecked() && computer_random == 0) ||
                         (btn_paper.isChecked() && computer_random == 1)) {
 
-                    tv_winner.setText("勝利者\n" + ed_name.getText().toString());
-                    tv_text.setText("恭喜您獲勝了！！！");
+                    tv_winner.setText("Winner\n" + ed_name.getText().toString());
+                    tv_text.setText("Palyer wins!");
 
                 } else if ((btn_scissor.isChecked() && computer_random == 1) ||
                         (btn_stone.isChecked() && computer_random == 2) ||
                         (btn_paper.isChecked() && computer_random == 0)) {
 
-                    tv_winner.setText("勝利者\n電腦");
-                    tv_text.setText("可惜，電腦獲勝了！");
+                    tv_winner.setText("Winner\nBot");
+                    tv_text.setText("Bot Wins");
 
                 } else {
-                    tv_winner.setText("勝利者\n平手");
-                    tv_text.setText("平局，請再試一次！");
+                    tv_winner.setText("Winner\n Tie");
+                    tv_text.setText("Tie, please try again");
                 }
             }
         });
